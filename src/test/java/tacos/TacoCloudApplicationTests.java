b30e14037e3ead973a212515b2757d3536d1a176
PisarevskiyRE <daimon--@live.ru>
2024-06-19 15:53:43 +0300

    chapter 2

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,13 +1,16 @@
 package tacos;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class TacoCloudApplicationTests {
+@ExtendWith(SpringExtension.class) // <1>
+@SpringBootTest                 // <2>
+public class TacoCloudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test                         // <3>
+  public void contextLoads() {
+  }
 
 }
